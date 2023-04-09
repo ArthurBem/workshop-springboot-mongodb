57a7db0b83b89b384150eab9fcfe3655676fe4ce
Arthur Bem <arthurcbem@gmail.com>
2023-04-09 15:14:37 -0300

    Inserção de usuário com post

--- src/main/java/com/educandoweb/mongodb/services/UserService.java
+++ src/main/java/com/educandoweb/mongodb/services/UserService.java
@@ -1,5 +1,7 @@
 package com.educandoweb.mongodb.services;
 
+
+
 import java.util.List;
 import java.util.Optional;
 
@@ -7,21 +9,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.mongodb.domain.User;
+import com.educandoweb.mongodb.dto.UserDTO;
 import com.educandoweb.mongodb.repositories.UserRepository;
 import com.educandoweb.mongodb.services.exception.ObjectNotFoundException;
 
+
 @Service
 public class UserService {
-    
-    @Autowired
-    private UserRepository repo;
 
-    public List<User> findAll(){
-        return repo.findAll();
-    }
+	@Autowired
+	private UserRepository repo;
 
-    public User findById(String id){
-        Optional<User> obj = repo.findById(id);
-        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-    }
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
