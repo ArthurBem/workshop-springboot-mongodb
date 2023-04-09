8dc80ead564ed997ceecafee90ab8571e4f95c8a
Arthur Bem <arthurcbem@gmail.com>
2023-04-09 14:44:18 -0300

    Obtendo um usuário por id

--- src/main/java/com/educandoweb/mongodb/services/UserService.java
+++ src/main/java/com/educandoweb/mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.educandoweb.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.mongodb.domain.User;
 import com.educandoweb.mongodb.repositories.UserRepository;
+import com.educandoweb.mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
